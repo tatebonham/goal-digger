27435827d0b04ae5439d9d1e5b3fa8bd2746e154
elek8871 <elekmichelle@gmail.com>
2022-10-07 16:44:07 -0600

    adds nav bar

--- src/components/partials/NavBar.js
+++ src/components/partials/NavBar.js
@@ -1,7 +1,17 @@
+import { Link, NavigationType } from "react-router-dom"
+
 export default function NavBar(){
     return(
-        <div>
-            NavBar Component
-        </div>
+        <nav>
+            <div>
+                
+                    <Link to = "/"> Home Page </Link> |
+                    <Link to ="/UserLogin"> Login </Link> |
+                    <Link to ="/UserNew"> Create New Account </Link> |
+                    <Link to ="/Profile"> Go To Your Profile </Link> |
+                    <Link to ="/"> LogOut </Link>
+               
+            </div>
+        </nav>
     )
 }
