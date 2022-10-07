cd925fbd6ca3ab195773951dde7de7ef4707654c
tatebonham <tatebonham@gmail.com>
2022-10-07 14:37:50 -0700

    Stubbed out routes on App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home'
+import GoalEdit from './components/pages/goals/GoalEdit'
+import GoalNew from './components/pages/goals/GoalNew'
+import Profile from './components/pages/users/Profile'
+import ProfileEdit from './components/pages/users/ProfileEdit'
+import UserNew from './components/pages/users/UserNew'
+import UserLogin from './components/pages/users/UserLogin'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route to='/' element={<Home />} />
+          <Route to='/user/new' element={<UserNew />} />
+          <Route to='/user/login' element={<UserLogin />} />
+          <Route to='/user/:id' element={<Profile />} />
+          <Route to='/user/:id/edit' element={<ProfileEdit />} />
+          <Route to='/goal/new' element={<GoalNew />} />
+          <Route to='/goal/:id/edit' element={<GoalEdit />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
