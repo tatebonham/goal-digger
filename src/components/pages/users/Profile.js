b0c287bd87123d514e8c47dde4459e65f353a4c4
Moodybleu <meganbenn27@icloud.com>
2022-10-10 13:45:11 -0700

    Starts profile page

--- src/components/pages/users/Profile.js
+++ src/components/pages/users/Profile.js
@@ -1,7 +1,24 @@
+import React from "react"
+import { useEffect, useState, Link } from "react-router-dom"
+import bootstrap from 'bootstrap'
+
+
 export default function Profile(){
+
+    const [goal, setGoal] = useState([])
+
+    const goalLink = goal.map(goal => {
+        return(
+            <div key={goal._id}>
+                <Link to={`/goal/${goal._id}`}>{goal.content}</Link>
+            </div>
+        )
+    })
+    goalLink()
+
     return(
         <div>
-            Profile Component
+            <h1>Welcome to your profile</h1>
         </div>
     )
 }
