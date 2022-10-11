7e69a21e5ef93315f02275f6852ca0ccc551e47e
Moodybleu <meganbenn27@icloud.com>
2022-10-11 09:51:23 -0700

    adds css

--- src/components/pages/goals/GoalEdit.js
+++ src/components/pages/goals/GoalEdit.js
@@ -18,7 +18,7 @@ export default function GoalEdit(){
     useEffect(() => {
         const getGoal = async () => {
         try {
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/goal/${id}`)
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/goal/${id}`)
             // console.log('TACO',response.data)
             setForm(response.data)
              } catch (err) {
