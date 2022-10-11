931cebd26fb1ccd9fcf382d7f55942a87b630940
tatebonham <tatebonham@gmail.com>
2022-10-11 12:12:30 -0700

    updated index.css requiring an image that didnt get uploaded, updated profile.js paths

--- src/components/pages/users/Profile.js
+++ src/components/pages/users/Profile.js
@@ -1,7 +1,6 @@
 import { Link} from 'react-router-dom' 
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import GoalCard from '../../partials/GoalCard'
 
 export default function Profile(){
 
@@ -33,7 +32,7 @@ export default function Profile(){
     const goalLink = goals.map(goal => {
         return(
             <div key={goal._id}>
-                <Link to={`/goal/${goal._id}`}>{goal.content}</Link>
+                <Link to={`/goal/${goal._id}/edit`}>{goal.content}</Link>
             </div>
         )
     })
@@ -43,10 +42,9 @@ export default function Profile(){
             <h1>Welcome to your profile</h1>
             {errorMessage}
             {/* create new goal */}
-            <Link to="/goals/GoalNew"> New Goals </Link> 
+            <Link to="/goal/new"> New Goals </Link> 
             {/* edit your profile */}
-            <Link to = "/users/profileEdit"> Edit yor profile</Link>
-            <GoalCard />
+            <Link to = "/users/edit"> Edit yor profile</Link>
             {goalLink}
     
         </div>
