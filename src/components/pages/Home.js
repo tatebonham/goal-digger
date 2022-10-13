1e622cc767a712418c6abf4b012cf9ce03192227
Shazammon <ammonorgill@gmail.com>
2022-10-12 18:29:43 -0600

    updated Apicomponent get inspired button and home.js file

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -33,9 +33,8 @@ export default function Home(){
     return(
     <div>
         <h1>Welcome To Goal Digger</h1>
-        <h3> Get inspired</h3>
         
-        <h2>{api}</h2>
+        <h2><ApiComp /></h2>
         <p>{errorMessage}</p>
         <CarouselFade />
     </div>
--- src/components/partials/ApiComp.js
+++ src/components/partials/ApiComp.js
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function NavBar() {
 
@@ -21,10 +22,27 @@ export default function NavBar() {
         }
         getApi()
     }, [])
+    
+    const updateApi = async (e) => {
+        e.preventDefault()
+        try{
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/bucketlist`)
+            // console.log(response.data)
+            setApi(response.data.item)
+        } catch(err) {
+            console.warn(err)
+            if(err.response) {
+                setErrorMessage(err.response.data.message)
+            }
+        }
+    }
 
 
     return(
         <div>
+            <form onSubmit={updateApi}>
+                <button type="submit"><h2>Get Inspired!</h2></button>
+            </form>
             <h2>{api}</h2>
         </div>
     )
