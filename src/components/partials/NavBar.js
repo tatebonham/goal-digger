31a4dfb5d566b51f706533ed29fbbb26de7f55a4
Moodybleu <meganbenn27@icloud.com>
2022-10-13 07:14:34 -0700

    Changes the navbar order so logout is the last item

--- src/components/partials/NavBar.js
+++ src/components/partials/NavBar.js
@@ -32,8 +32,8 @@ export default function NavBar({currentUser, setCurrentUser}){
         <nav>
             <div>
                 <Link to ="/"> Home </Link> 
-                {currentUser ? loggedIn : loggedOut}
                 <Link to ="/goal/new"> Create New Goal</Link>
+                {currentUser ? loggedIn : loggedOut}
             </div>
         </nav>
     )
